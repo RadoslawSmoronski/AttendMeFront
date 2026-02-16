e7cd8be402a3f27f34961db20cdc2ea5585aa009
RadoslawSmoronski <radoslaw.smo@gmail.com>
2026-02-16 17:55:34 +0100

    feat: login view UI and basic API integration #6

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="container mt-5">
-    <div class="card text-center">
-      <div class="card-header">Test Bootstrap</div>
-      <div class="card-body">
-        <h5 class="card-title">Its working!</h5>
-        <p class="card-text">Vue 3 + Bootstrap 5</p>
-        <button class="btn btn-primary">test</button>
-      </div>
-    </div>
-  </div>
+  <router-view></router-view>
 </template>
 
-<style scoped></style>
+<script setup lang="ts"></script>

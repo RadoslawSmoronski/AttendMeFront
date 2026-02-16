cb601738fdb0b3d60239a03057cd5a4754083e14
RadoslawSmoronski <radoslaw.smo@gmail.com>
2026-02-16 12:32:14 +0100

    chore: setup project structure and install bootstrap (Closes #2, Closes #3)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container mt-5">
+    <div class="card text-center">
+      <div class="card-header">Test Bootstrap</div>
+      <div class="card-body">
+        <h5 class="card-title">Its working!</h5>
+        <p class="card-text">Vue 3 + Bootstrap 5</p>
+        <button class="btn btn-primary">test</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
